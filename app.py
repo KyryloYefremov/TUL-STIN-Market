b5e093d1703e697d732442f8e640c9e0956e1643
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-03-17 17:54:05 +0100

    add endpoints /liststock and /salestock

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
@@ -7,6 +7,35 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/liststock')
+def send_liststock():
+    """
+    Send a list of filterd stocks to the endpoint as json.
+    """
+    json_data = [
+        {"name": "Microsoft", "date": None, "rating": None, "sale": None},
+        {"name": "Google", "date": None, "rating": None, "sale": None},
+        {"name": "OpenAI", "date": None, "rating": None, "sale": None},
+    ]
+    return jsonify(json_data)
+
+
+@app.route('/salestock')
+def add_recommendations():
+    """ 
+    Add a recommendation "to sell" to those items
+    that have a higher rainging than the user-defined value. 
+    Send the updated json to the endpoint
+    """
+    json_data = [
+        {"name": "Microsoft", "date": None, "rating": None, "sale": 1},
+        {"name": "Google", "date": None, "rating": None, "sale": 0},
+        {"name": "OpenAI", "date": None, "rating": None, "sale": 0},
+    ]
+    return jsonify(json_data)
+
 
 
 if __name__ == '__main__':
