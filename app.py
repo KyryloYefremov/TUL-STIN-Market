167299a3233343d10d1f4cfe4c7ba22c8ba17fa5
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-04-02 22:08:19 +0200

    Add scheduler to trigger defined time intervals

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 from flask import Flask, render_template, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+def update_stock_data():
+    """
+    Function to update stock data. This function will be called by the scheduler or manually from UI.
+    The function will trigger the pipeline:
+    1. Get the stock data from the API
+    2. Filter the data
+    3. Send data to modul "News" to get ratings for requested companies stockes based on their latest news.
+    4. Based on the ratings, add a recommendation to the user favorite stockes either to sell, or keep them.
+    5. Send the updated stock data to the modul "News".
+    """
+    print("Stock data updated!")
+
 
 
 app = Flask(__name__)
+
+scheduler = BackgroundScheduler()  # initialize the scheduler
+# create a job to update stock data at defined time intervals
+scheduler.add_job(
+    update_stock_data,
+    trigger=CronTrigger(hour='0, 6, 12, 18', minute='0'),
+    id='update_stock_data',
+    replace_existing=True,
+)
+scheduler.start()
 
 
 @app.route('/')
